65ad99028f17933e6832759f4061024b61218a34
Benjamín Gazale <benjamingazale@gmail.com>
2026-04-23 13:31:38 -0400

    Agregando archivo faltante y actualizaciones

---
+++ funciones.h
@@ -0,0 +1,37 @@
+/* definiciones de los datos y operaciones de gestión de memoria y lectura
+Código página 5 y 6 de las intruccione
+*/
+
+#define infinito 2147000000
+
+typedef int **Matriz;
+typedef int *Arreglo;
+typedef bool *ArregloBool;
+
+ArregloBool crearArregloBool(int n){
+    ArregloBool aux;
+    int i;
+    aux = callov(n, ssizeof(bool));
+    if(aux == NULL){
+        printf("No hay memoria suficiente. ", 160);
+        exit(0);
+    }
+    return aux;
+}
+Arreglo crearArreglo(int n){
+    Arreglo aux;
+    int i;
+
+    aux = calloc(n,sizeof(int));
+    if (aux == NULL)
+    {
+        printf("No hay memoria suficiente. ", 160);
+        exit(0);
+    }
+    return aux;
+}
+//Esto e suna prueba
+
+
+
+
